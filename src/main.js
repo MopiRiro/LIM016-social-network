14efeb5416fe644956b5e6e2cdfec5e8b6e573d4
unknown <nataliabravojimenez@gmail.com>
2021-12-03 08:19:22 -0500

    validación de correo y contraseña

--- src/main.js
+++ src/main.js
@@ -11,3 +11,29 @@ let navList= document.querySelector(".navList");
 toggleBtn.addEventListener("click", ()=>{
     navList.classList.toggle("navVisible")
 })
+
+const signInForm= document.querySelector(".formSignIn");
+const userEmail= document.getElementById("inputUserEmail");
+const userPassword= document.getElementById("inputUserPassword");
+
+signInForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+
+    if(userEmail.value==""|| userPassword.value==""){
+      
+        alert("no debes dejar campos vacios");
+    }
+    let passwordRegEx= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    let emailRegEx= /\S+@\S+\.\S+/;
+    if(emailRegEx.test(userEmail.value)== false){
+        alert("coloca tu correo completo")
+    }
+    if(passwordRegEx.test(userPassword.value)==false){
+        alert(" Tu contraseña debe tener Mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial")
+    }
+
+    if(emailRegEx.test(userEmail.value)== true && passwordRegEx.test(userPassword.value)==true){
+        alert("inicio de sesión exitoso");
+    }
+})
