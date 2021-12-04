0c4858adf3a9aa3675743fad3a13d92bafca73e1
unknown <nataliabravojimenez@gmail.com>
2021-12-04 18:33:44 -0500

    sign up versión desktop

--- src/view/signup.js
+++ src/view/signup.js
@@ -1,24 +1,30 @@
 export default () => {
   const viewSignUp = `
-    <div class="containerTitleSignIn">
-        <h2>Sign Up</h2>
-    </div>
-    <div class="containerUserData">
-        <form action="" class="signUpForm">
-            <input type="email" placeholder="Email" class="inputUserData"/>
-            <input type="password" placeholder="Password" class="inputUserData"/>
-            <div class="containerBtn">
-                <button class="btn">
-                    <a href="#/">Sign Up</a>
-                </button>
-                <p>O ingresa con... </p>
-            </div>
-        </form>
-    </div>
-   <div class="containerBtnSocialNetworks">
-        <button class="btnSocialNetworks"><i class="fab fa-facebook"></i></button>
-        <button class="btnSocialNetworks"><i class="fab fa-google-plus-g"></i></button>
-   </div> 
+  <div class="containerImgSignUp">
+  <img src="../src/img/imgClaqueta.png">
+</div> 
+
+<div class ="containerUserSignUp">
+  <div class="containerTitleSignIn">
+    <h2>Sign Up</h2>
+  </div>
+<div class="containerUserData">
+  <form action="" class="signUpForm">
+      <input type="email" placeholder="Email" class="inputUserData"/>
+      <input type="password" placeholder="Password" class="inputUserData"/>
+      <div class="containerBtn">
+          <button class="btn">
+              <a href="#/">Sign Up</a>
+          </button>
+          <p>O ingresa con... </p>
+      </div>
+  </form>
+</div>
+<div class="containerBtnSocialNetworks">
+    <button class="btnSocialNetworks"><i class="fab fa-facebook"></i></button>
+    <button class="btnSocialNetworks"><i class="fab fa-google-plus-g"></i></button>
+</div>      
+</div> 
    `;
   const sectionView = document.createElement('section');
   sectionView.classList.add('containerSignUp');
