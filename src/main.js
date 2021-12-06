ad3a576be197bfb5130e458bfee88011f02c40be
unknown <nataliabravojimenez@gmail.com>
2021-12-06 12:41:13 -0500

    validación de formulario SPA

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-/* eslint-disable implicit-arrow-linebreak */
-// // Este es el punto de entrada de tu aplicacion
-// import { myFunction } from './lib/index.js';
-// myFunction();
 import { changeViews } from './view-controller/router.js';
+// import signIn from './view/signin.js';
 
 const init = () => {
   changeViews(window.location.hash);
   window.addEventListener('hashchange', () =>
-    // eslint-disable-next-line implicit-arrow-linebreak
     // eslint-disable-next-line comma-dangle
+    // eslint-disable-next-line implicit-arrow-linebreak
     changeViews(window.location.hash));
 };
 
@@ -21,56 +18,56 @@ toggleBtn.addEventListener('click', () => {
   navList.classList.toggle('navVisible');
 });
 
-const signInForm = document.querySelector('.formSignIn');
-const userEmail = document.getElementById('inputUserEmail');
-const userPassword = document.getElementById('inputUserPassword');
+// eslint-disable-next-line no-console
+// console.log(signInForm);
 
-const showModal = (message) => {
-  const modalBox = document.getElementById('modal');
-  modalBox.style.display = 'block';
-  modalBox.innerHTML = `
-    <div class='modalContent'>
-    <p class='modalText'>${message}</p>
-    <button class='closeModalBtn'>Entendido</button>
-    </div>`;
+// const showModal = (message) => {
+//   const modalBox = document.getElementById('modal');
+//   modalBox.style.display = 'block';
+//   modalBox.innerHTML = `
+//     <div class='modalContent'>
+//     <p class='modalText'>${message}</p>
+//     <button class='closeModalBtn'>Entendido</button>
+//     </div>`;
 
-  const modalClose = document.querySelector('.closeModalBtn');
-  modalClose.addEventListener('click', () => {
-    modalBox.style.display = 'none';
-  });
+//   const modalClose = document.querySelector('.closeModalBtn');
+//   modalClose.addEventListener('click', () => {
+//     modalBox.style.display = 'none';
+//   });
 
-  window.addEventListener('click', (e) => {
-    if (e.target === modalBox) {
-      modalBox.style.display = 'none';
-    }
-  });
-};
+//   window.addEventListener('click', (e) => {
+//     if (e.target === modalBox) {
+//       modalBox.style.display = 'none';
+//     }
+//   });
+// };
 
-signInForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+// signInForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
 
-  const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  const emailRegEx = /\S+@\S+\.\S+/;
+//   const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+//   const emailRegEx = /\S+@\S+\.\S+/;
 
-  if (userEmail.value === '' || userPassword.value === '') {
-    e.preventDefault();
-    showModal('No puedes dejar espacios en blanco');
-  } else if (emailRegEx.test(userEmail.value) === false) {
-    e.preventDefault();
-    showModal('Coloca tu correo completo');
-  } else if (passwordRegEx.test(userPassword.value) === false) {
-    e.preventDefault();
-    showModal(
-      // eslint-disable-next-line comma-dangle
-      // eslint-disable-next-line max-len
-      // eslint-disable-next-line function-paren-newline
-      'Tu contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial');
-  } else if (
-    emailRegEx.test(userEmail.value) === true
-    && passwordRegEx.test(userPassword.value) === true
-  ) {
-    e.preventDefault();
-    showModal('Inicio de sesión exitoso');
-    // signInForm.reset();
-  }
-});
+//   if (userEmail.value === '' || userPassword.value === '') {
+//     e.preventDefault();
+//     showModal('No puedes dejar espacios en blanco');
+//   } else if (emailRegEx.test(userEmail.value) === false) {
+//     e.preventDefault();
+//     showModal('Coloca tu correo completo');
+//   } else if (passwordRegEx.test(userPassword.value) === false) {
+//     e.preventDefault();
+//     showModal(
+//       // eslint-disable-next-line comma-dangle
+//       // eslint-disable-next-line max-len
+//       // eslint-disable-next-line function-paren-newline
+// eslint-disable-next-line max-len
+//       'Tu contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial');
+//   } else if (
+//     emailRegEx.test(userEmail.value) === true
+//     && passwordRegEx.test(userPassword.value) === true
+//   ) {
+//     e.preventDefault();
+//     showModal('Inicio de sesión exitoso');
+//     // signInForm.reset();
+//   }
+// });
--- src/view/signin.js
+++ src/view/signin.js
@@ -1,6 +1,5 @@
 export default () => {
   const viewSignIn = `      
-    
         <div class="containerImgSignIn">
             <img src="../src/img/imgClaqueta.png">
         </div>
@@ -15,7 +14,7 @@ export default () => {
                     <input type="password" placeholder="Password" class="inputUserData" id="inputUserPassword"/>
                     <div class="containerBtn">
                         <button type="submit" class="btn">
-                        <a href="#/timeline">Sign In</a>
+                        <a>Sign In</a>
                         </button>
                         <p>O ingresa con... </p>
                     </div>
@@ -33,10 +32,64 @@ export default () => {
             </div>
         </div>   
     
+        <section class="modal" id="modal">
+        </section>
     
  `;
   const sectionView = document.createElement('section');
   sectionView.classList.add('containerSignIn');
   sectionView.innerHTML = viewSignIn;
+
+  const showModal = (message) => {
+    const modalBox = sectionView.querySelector('#modal');
+    modalBox.style.display = 'block';
+    modalBox.innerHTML = `
+      <div class='modalContent'>
+      <p class='modalText'>${message}</p>
+      <button class='closeModalBtn'>Entendido</button>
+      </div>`;
+    const modalClose = sectionView.querySelector('.closeModalBtn');
+    modalClose.addEventListener('click', () => {
+      modalBox.style.display = 'none';
+    });
+    window.addEventListener('click', (e) => {
+      if (e.target === modalBox) {
+        modalBox.style.display = 'none';
+      }
+    });
+  };
+  const signInForm = sectionView.querySelector('.formSignIn');
+  const userEmail = sectionView.querySelector('#inputUserEmail');
+  const userPassword = sectionView.querySelector('#inputUserPassword');
+
+  signInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const emailRegEx = /\S+@\S+\.\S+/;
+
+    if (userEmail.value === '' || userPassword.value === '') {
+      e.preventDefault();
+      showModal('No puedes dejar espacios en blanco');
+    } else if (emailRegEx.test(userEmail.value) === false) {
+      e.preventDefault();
+      showModal('Coloca tu correo completo');
+    } else if (passwordRegEx.test(userPassword.value) === false) {
+      e.preventDefault();
+      showModal(
+        // eslint-disable-next-line comma-dangle
+        // eslint-disable-next-line max-len
+        // eslint-disable-next-line function-paren-newline
+        'Tu contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial');
+    } else if (
+      emailRegEx.test(userEmail.value) === true
+      && passwordRegEx.test(userPassword.value) === true
+    ) {
+      e.preventDefault();
+      window.location.hash = '#/timeline';
+      // signInForm.reset();
+    }
+  });
+
   return sectionView;
 };
