1839376e59ac6bb6374f7b1c30836430b2824e4e
MopiRiro <riveraromeromonicadelpilar@gmail.com>
2021-12-15 12:05:55 -0500

    Implementacion Firestore

--- src/view/timeline.js
+++ src/view/timeline.js
@@ -1,5 +1,13 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-console */
 import { signOutUser } from '../lib/auth.js';
+import {
+  createPost,
+  getPost,
+  updatePost,
+  getPostNow,
+  deletePost,
+} from '../lib/firestore.js';
 
 export default () => {
   const viewTimeLine = `
@@ -54,21 +62,10 @@ export default () => {
         </div>
     </div>
     <div class="containerOthersPosts">
-        <div class="othersInfo">
-            <p>Jean</p>
-            <i class="fa fa-times" aria-hidden="true"></i>
-        </div>
-        <div class="othersToPost">
-            <input type="text" class="othersToPostInput" value="Hi"> </input>
-        </div>
-        <div class="likeAndShare">
-            <i class="fa fa-heart circleIcon" aria-hidden="true"></i>
-            <i class="fa fa-paper-plane circleIcon" aria-hidden="true"></i>
-            <i class="fa fa-pencil circleIcon" aria-hidden="true"></i>
-            <i class="fa fa-trash circleIcon" aria-hidden="true"></i>
-        </div>
+        
     </div>
 </div>
+<button id="btnPrueba">Prueba</button>
 <button id="myBtn" title="Go to top"><i class="fas fa-arrow-up"></i></button>
     </main>
    `;
@@ -89,6 +86,82 @@ export default () => {
       console.log(error);
     // An error happened.
     });
+  });
+
+  // FireStore
+  const containerUsersPosts = sectionView.querySelector('.containerOthersPosts');
+  const btnShare = sectionView.querySelector('.shareBtn');
+  const toPostInput = sectionView.querySelector('.toPostInput');
+  let idPost = '';
+  let editStatus = false;
+  const btnPrueba = sectionView.querySelector('#btnPrueba');
+  btnPrueba.addEventListener('click', () => {
+    getPostNow((snapshot) => {
+      containerUsersPosts.innerHTML = '';
+      snapshot.docs.forEach((doc) => {
+        const publication = doc.data();
+        publication.id = doc.id;
+        containerUsersPosts.innerHTML += `
+        <div class="othersInfo">
+          <p>Jean</p>
+          <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <div class="othersToPost">
+          <input type="text" class="othersToPostInput" value="${publication.description}"> </input>
+        </div>
+        <div class="likeAndShare">
+          <i class="fa fa-heart circleIcon" aria-hidden="true"></i>
+          <i class="fa fa-paper-plane circleIcon" aria-hidden="true"></i>
+          <i class="fa fa-pencil circleIcon" aria-hidden="true" data-id="${publication.id}" id="btnEdit"></i>
+          <i class="fa fa-trash circleIcon" aria-hidden="true" data-id="${publication.id}" id="btnDelete"></i>
+        </div>`;
+        const btnDelete = sectionView.querySelectorAll('#btnDelete');
+        btnDelete.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            // eslint-disable-next-line no-alert
+            confirm('Quieres Eliminar');
+            deletePost(e.target.dataset.id).then(() => {
+
+            }).catch((error) => {
+              console(error.message);
+            });
+          });
+        });
+        const btnEdit = sectionView.querySelectorAll('#btnEdit');
+        btnEdit.forEach((btn) => {
+          btn.addEventListener('click', (e) => {
+            getPost(e.target.dataset.id).then((docSnap) => {
+              if (docSnap.exists()) {
+                const postEditUser = docSnap.data();
+                editStatus = true;
+                idPost = docSnap.id;
+                toPostInput.value = postEditUser.description;
+                btnShare.innerText = 'Editar';
+              } else {
+                console.log('no se encontro documento');
+              }
+            });
+          });
+        });
+      });
+    });
+  });
+
+  btnShare.addEventListener('click', () => {
+    const postShareUser = toPostInput.value;
+    if (!editStatus) {
+      createPost(postShareUser).then(() => {
+        // getPosts();
+        // postShareUser.innerHTML = '';
+      }).catch((error) => console.log(error.message));
+    } else {
+      updatePost(idPost, {
+        description: postShareUser,
+      }).then(() => {
+        // postShareUser.innerHTML = '';
+        console.log('se edito');
+      }).catch((error) => console.log(error.message));
+    }
   });
   return sectionView;
 };
